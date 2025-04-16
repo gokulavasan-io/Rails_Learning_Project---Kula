74e3942e352b57d12a08bf519909869a171c8d32
gokulavasan-io <gokulavasanoff@gmail.com>
2025-04-16 23:33:33 +0530

    added association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :orders, dependent: :destroy
 end
