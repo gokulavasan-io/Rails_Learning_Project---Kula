ae9345b7025bb0a9bb7fe8ff744e51c1c3fb8ebc
gokulavasan-io <gokulavasanoff@gmail.com>
2025-04-16 17:40:40 +0530

    created orders - model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_16_113640) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_16_120813) do
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -26,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_16_113640) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
   end
+
+  add_foreign_key "orders", "users"
 end
