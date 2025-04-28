af001f9998cce804a649b14d85b12943616bc500
Gokulavasan Govindan <gokulavasan@temps-MacBook-Pro.local>
2025-04-28 13:30:34 +0530

    implemented caching

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::OrdersController < ApplicationController
   before_action :set_order, only: [ :show, :update, :destroy ]
 
   def index
-    orders = @current_user.orders.includes(order_items: :product)
+    orders = Rails.cache.fetch("users/#{@current_user.id}/orders", expires_in: 10.minutes) do
+      @current_user.orders.includes(order_items: :product).to_a
+    end
     render json: serialize_order(orders), status: :ok
   end
 
@@ -15,24 +17,37 @@ class Api::V1::OrdersController < ApplicationController
     order.user = @current_user
     order.save!
 
+    Rails.cache.delete("users/#{@current_user.id}/orders")
+    Rails.cache.write("orders/#{order.id}", order)
+
     OrderProcessingJob.perform_async(order.id)
     render json: { message: "Order placed successfully" }, status: :created
   end
 
   def update
     @order.update!(order_params)
+
+    Rails.cache.delete("users/#{@current_user.id}/orders")
+    Rails.cache.delete("orders/#{@order.id}")
+
     render json: { message: "Order updated successfully" }, status: :ok
   end
 
   def destroy
     @order.destroy
+
+    Rails.cache.delete("users/#{@current_user.id}/orders")
+    Rails.cache.delete("orders/#{@order.id}")
+
     head :no_content
   end
 
   private
 
   def set_order
-    @order = Order.includes(order_items: :product).find(params[:id])
+    @order = Rails.cache.fetch("orders/#{params[:id]}", expires_in: 10.minutes) do
+      Order.includes(order_items: :product).find(params[:id])
+    end
   end
 
   def order_params
